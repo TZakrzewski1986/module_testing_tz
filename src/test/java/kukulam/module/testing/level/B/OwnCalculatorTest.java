bf3e19d5e99e1aa9aaf5acb50d9032cacae505fd
Tomasz Zakrzewski <tomasz.zakrzewski86@gmail.com>
2020-04-29 12:17:27 +0200

    Commit 12:20 29.04.2020

--- src/test/java/kukulam/module/testing/level/B/OwnCalculatorTest.java
+++ src/test/java/kukulam/module/testing/level/B/OwnCalculatorTest.java
@@ -4,8 +4,27 @@ package kukulam.module.testing.level.B;
     simple test (self implementation)
  */
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 /**
  * TODO: write own implementation of {@link kukulam.module.testing.level.B.OwnCalculator}
  * Create class OwnCalculatorTest
  * Hint: all necessary examples are in {@link kukulam.module.testing.level.A.IntegerCalculatorTest}
  */
+public class OwnCalculatorTest {
+
+    @Test
+    void shouldAddCorrectlyThreeInts() {
+        // Given
+        OwnCalculator ownCalculator = new OwnCalculator();
+
+        // When
+        int result = ownCalculator.add(2, 2,6);
+
+        // Then
+        assertThat(result).isEqualTo(10);
+
+    }
+}
